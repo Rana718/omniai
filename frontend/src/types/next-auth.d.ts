c6ff306787c7a55424f0df2dc521f30525d666f9
Rana718 <ranadolui718@gmail.com>
2026-07-07 21:23:45 +0530

    fix lint

--- frontend/src/types/next-auth.d.ts
+++ frontend/src/types/next-auth.d.ts
@@ -1,3 +1,4 @@
+import type { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
     interface Session {
@@ -6,7 +7,7 @@ declare module "next-auth" {
             name: string;
             image?: string;
             accessToken: string;
-        };
+        } & DefaultSession["user"];
     }
 
     interface User {
